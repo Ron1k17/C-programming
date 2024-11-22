d9cac4c4f218ad7c540c58412ae09eb639a5044d
Ron1k17 <ronik.workspace@gmail.com>
2024-11-22 19:19:39 +0530

    Adding C++ programming folder

---
+++ demo.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int x;
+//     cin>>x;
+//     cout<<"hey"<<endl<<x;
+//     cout<<"C++ is working";
+// }
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World" << endl;
+    int a;
+    int b;
+
+    cin >> a;
+    cout << "enter number a:"<<a<<endl;
+    cin >> b;
+
+    cout << "enter number b:"<<b<<endl;
+
+    int sum = a+b;
+    cout<< "sum of a+b :"<<sum;
+
+
+}
---
+++ demo1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+    cout<<"hey"<<endl<<x;
+    cout<<"C++ is working";
+}
